7cb930fd4e6afd40b7274e6bd515b8ee9e1b5cab
Blake Runyon <blakerunyon09@gmail.com>
2021-01-03 19:54:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,12 +16,34 @@ def game_hash
           :steals => "",
           :blocks => "",
           :slam_dunks => ""
+        },
+        {
+          :player_name => "",
+          :number => "",
+          :shoe => "",
+          :points => "",
+          :rebounds => "",
+          :assists => "",
+          :steals => "",
+          :blocks => "",
+          :slam_dunks => ""
         }
     },
     :away => {
       :team_name => "Charlotte Hornet",
       :colors => ["Turquoise", "Purple"],
       :players => [
+        {
+          :player_name => "",
+          :number => "",
+          :shoe => "",
+          :points => "",
+          :rebounds => "",
+          :assists => "",
+          :steals => "",
+          :blocks => "",
+          :slam_dunks => ""
+        },
         {
           :player_name => "",
           :number => "",
