5afada6486f8f1dd2e5774a8dd5a30dd1b314858
Blake Runyon <blakerunyon09@gmail.com>
2021-01-03 19:47:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,9 +4,13 @@ def game_hash
   hash = {
     :home => {
       :team_name => "Brooklyn Nets"
+      :colors => "Black, White"
+      :players => "Brooklyn Nets"
     },
     :away => {
       :team_name => "Charlotte Hornet"
+      :colors => "Brooklyn Nets"
+      :players => "Brooklyn Nets"
     }
   }
 end
